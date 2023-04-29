e5817ed85cad675951cd08e5b192f3d857905050
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-04-30 05:39:53 +0900

    [clean]現在修正中

--- Day5/prob5-3/counter.cpp
+++ Day5/prob5-3/counter.cpp
@@ -1,13 +1,13 @@
 #include "counter.h"
 
 Counter::Counter() : m_count(0){
+  m_totalCount = 0;
 }
 void Counter::reset(){
   m_count = 0;
 }
 void Counter::count(){
   m_count++;
-  m_totalCount++;
 }
 int Counter::getCount(){
   return m_count;
