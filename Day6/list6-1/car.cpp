bf2111b20b6e2a795277df0b1d5da6320813978d
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-05-06 08:49:25 +0900

    upload Day6

---
+++ Day6/list6-1/car.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "car.h"
+
+CCar::CCar() : m_fuel(0),m_migration(0){
+  std::cout << "CCarオブジェクト生成" << std::endl;
+}
+CCar::~CCar(){
+  std::cout << "CCarオブジェクト破棄" << std::endl;
+}
+void CCar::move(){
+  if(m_fuel >= 0){
+    m_migration++;
+    m_fuel--;
+  }
+  std::cout << "移動距離:" << m_migration << std::endl;
+  std::cout << "燃料:" << m_fuel << std::endl;
+}
+void CCar::supply(int fuel){
+  if(fuel > 0){
+    m_fuel += fuel;
+  }
+  std::cout << "燃料;" << m_fuel << std::endl;
+}
---
+++ Day6/list6-2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Position2D.h"
+#include "Position2D.cpp"
+
+int main(int argc, char** args){
+  Position2D p;
+  p.setValue(1,1);
+  p.move(2,3);
+  std::cout << "p:(" << p.getX() << "," << p.getY() << ")" << std::endl;
+  p.resetPosition();
+  std::cout << "p:(" << p.getX() << "," << p.getY() << ")" << std::endl;
+  return 0;
+}
