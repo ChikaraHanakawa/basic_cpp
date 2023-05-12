c71a6cae65fb660129c04b2daba2b68befa2abee
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-05-13 02:20:58 +0900

    upload Day8

--- Day8/probex1-3/main.cpp
+++ Day8/probex1-3/main.cpp
@@ -4,13 +4,13 @@ void swap(int&, int&);
 
 int main(){
   int a = 1, b = 2;
-  std::cout << "a = " << a << "b = " << b << std::endl;
+  std::cout << "a = " << a << " b = " << b << std::endl;
   swap(a ,b);
-  std::cout << "a = " << a << "b = " << b << std::endl;
+  std::cout << "a = " << a << " b = " << b << std::endl;
   return 0;
 }
 
-void swap(int a, int b){
+void swap(int &a, int &b){
   int tmp;
   tmp = a;
   a = b;
