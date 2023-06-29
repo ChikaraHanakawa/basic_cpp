1ea250993b25b3ddb270f08b51b90cfa748fcdfc
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-06-29 18:00:34 +0900

    update

--- try_prog/DP_reco.cpp
+++ try_prog/DP_reco.cpp
@@ -5,33 +5,42 @@
 #include <vector>
 #include <filesystem>
 
-std::string file_number1, file_number2, filename_tmp="/city/city_.txt";
+std::string file_number1, file_number2;
+std::string filename_tmp="/city/city_.txt";
+std::string directory_pas1, directory_pas2;
 
 class DPmatting{
   private:
+    std::vector<std::string> template_data = {};
   public:
     void fileopen();
     void calculation();
-}
-void DPmatting::fileopen(std::string directory_pas1, std::string directory_pas2){
-  std::filesystem::directory_iterator e = std::filesystem::directory_iterator(directory_pas);
-  for(auto f : e){
-    f.path().string();
-  }
-}
-int main(){
-  std::cin << file_number1;
-  std::cin << file_number2;
-  if(file_number1 >= 100 && file_number2 >= 100){
-    std::cout << "Fuck!!!" << std::endl;
-  }
-  std::string directory_pas1 = filename_tmp.insert(5, file_number1);
-  directory_pas1.insert(13, filenumber1);
-  directory_pas1.insert(18, filenumber1);
-  std::string directory_pas2 = filename_tmp.insert(5, file_number1);13m18
-  directory_pas2.insert(13, filenumber2);
-  directory_pas2.insert(18. filenumber2);
+};
 
-  DPmatting *ma;
-  ma->fileopen(directory_pas1, directory_pas2);
+void DPmatting::fileopen(){
+  std::filesystem::directory_iterator e = std::filesystem::directory_iterator(directory_pas1);
+  for(auto f : e){
+    if(f <= 2){
+      template_data[] = f.path().string();
+    }
+  }
+  std::cout << template_data[] << std::endl;
+  //return template_data[];
+}
+/*
+void DPmatting::calculation(){
+}*/
+
+int main(){
+  std::cin >> file_number1;
+  std::cin >> file_number2;
+  directory_pas1 = filename_tmp.insert(5, file_number1);
+  directory_pas1.insert(13, file_number1);
+  directory_pas1.insert(18, file_number1);
+  directory_pas2 = filename_tmp.insert(5, file_number1);
+  directory_pas2.insert(13, file_number2);
+  directory_pas2.insert(18, file_number2);
+
+  DPmatting d;
+  d.fileopen();
 }
