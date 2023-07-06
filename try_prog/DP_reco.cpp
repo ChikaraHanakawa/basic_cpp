7a424f77824603b77821f6a43bd6f8528c4ac767
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-07-07 02:36:05 +0900

    update

--- try_prog/DP_reco.cpp
+++ try_prog/DP_reco.cpp
@@ -3,44 +3,50 @@
 #include <string>
 #include <sstream>
 #include <vector>
-#include <filesystem>
+#include <experimental/filesystem>  // 追加
 
 std::string file_number1, file_number2;
-std::string filename_tmp="/city/city_.txt";
+std::string filename_tmp = "/city/city_.txt";
 std::string directory_pas1, directory_pas2;
 
-class DPmatting{
-  private:
-    std::vector<std::string> template_data = {};
-  public:
+class DPmatting {
+private:
+    std::vector<std::string> template_data;
+
+public:
     void fileopen();
     void calculation();
 };
 
-void DPmatting::fileopen(){
-  std::filesystem::directory_iterator e = std::filesystem::directory_iterator(directory_pas1);
-  for(auto f : e){
-    if(f <= 2){
-      template_data[] = f.path().string();
+void DPmatting::fileopen() {
+    std::experimental::filesystem::recursive_directory_iterator e(directory_pas1);  // 変更
+    int count = 0;  // ファイルの個数をカウントする変数
+    for (auto f : e) {
+        if (count <= 2) {
+            template_data.push_back(f.path().string());
+            count++;
+        }
     }
-  }
-  std::cout << template_data[] << std::endl;
-  //return template_data[];
+    std::cout << template_data[0] << std::endl;
 }
-/*
-void DPmatting::calculation(){
-}*/
 
-int main(){
-  std::cin >> file_number1;
-  std::cin >> file_number2;
-  directory_pas1 = filename_tmp.insert(5, file_number1);
-  directory_pas1.insert(13, file_number1);
-  directory_pas1.insert(18, file_number1);
-  directory_pas2 = filename_tmp.insert(5, file_number1);
-  directory_pas2.insert(13, file_number2);
-  directory_pas2.insert(18, file_number2);
-
-  DPmatting d;
-  d.fileopen();
+void DPmatting::calculation() {
+    // ファイルの計算処理を実装する
 }
+
+int main() {
+    std::cin >> file_number1;
+    std::cin >> file_number2;
+    directory_pas1 = filename_tmp.insert(5, file_number1);
+    directory_pas1.insert(13, file_number1);
+    directory_pas1.insert(18, file_number1);
+    directory_pas2 = filename_tmp.insert(5, file_number1);
+    directory_pas2.insert(13, file_number2);
+    directory_pas2.insert(18, file_number2);
+
+    DPmatting d;
+    d.fileopen();
+
+    return 0;
+}
+
