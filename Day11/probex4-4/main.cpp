deda6442705234d80fef261114a0faaeb2335b97
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-11-13 20:42:08 +0900

    update Day11

--- Day11/probex4-4/main.cpp
+++ Day11/probex4-4/main.cpp
@@ -4,39 +4,46 @@
 
 int main(){
     int count = 0;
-    int max_cou = 0;
-    int min_cou = 0;
-    int st_max;
+    int st_max = 0;
     int st_min;
     std::string input;
     std::string max;
     std::string min;
+    std::vector<int> max_cou;
+    std::vector<int> min_cou;
     std::vector<std::string> v1;
-    std::vector<std::string> vec_max;
-    std::vector<std::string> vec_min;
+
     while(1){
         std::cout << "文字列を入力:";
-        std::cin >> input;
+        std::getline(std::cin, input);
         v1.push_back(input);
-        if(input.length() == 0){
-            break;
-        }
-        if(st_max > v1[count].length()){
+        if(input.empty()) break;
+        if(v1[count].length() > st_max){
             st_max = v1[count].length();
             max = v1[count];
-        }else if(st_max == v1[count].length()){
-            vec_max.push_back(v1[count]);
-            max_cou++;
+        }else if(v1[count].length() == st_max){
+            max_cou.push_back(count);
         }
-        if(st_min < v1[count].length()){
+        if(v1[count].length() < st_min){
             st_min = v1[count].length();
             min = v1[count];
-        }else if(st_min == v1[count].length()){
-            vec_min.push_back(v1[count]);
-            min_cou++;
+        }else if(v1[count].length() == st_min){
+            min_cou.push_back(count);
         }
         count++;
     }
-    std::cout << "最長の単語:" << max << vec_max[max_cou] << std::endl;
-    std::cout << "最短の単語:" << min << vec_min[min_cou] << std::endl;
+    std::cout << "最長の単語:" << max;
+    for(int i = 0;i < max_cou.size();i++){
+        if(v1[max_cou[i]].length() == max.length()){
+            std::cout << " " << v1[max_cou[i]];
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "最短の単語:" << min;
+    for(int i = 0;i < min_cou.size();i++){
+        if(v1[min_cou[i]].length() == min.length()){
+            std::cout << " " << v1[min_cou[i]];
+        }
+    }
+    std::cout << std::endl;
 }
