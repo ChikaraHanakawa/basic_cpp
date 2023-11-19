711e025c0e01279c0afc81870f6d949f1266052d
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-11-19 14:36:12 +0900

    upload prob4-6

--- Day11/probex4-6/main.cpp
+++ Day11/probex4-6/main.cpp
@@ -7,5 +7,13 @@ int main(){
     while(1){
         std::cout << "正の整数を入力:";
         std::cin >> input;
+        if(input == -1) break;
+        li.push_back(input);
+        li.sort();
+        std::cout << "入力した数値:";
+        for(auto itr = li.begin(); itr != li.end(); ++itr){
+            std::cout << *itr << " ";
+        }
+        std::cout << std::endl;
     }
 }
