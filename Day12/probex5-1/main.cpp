afde10770d7f73cbb2d24300a450b9c2165f6bca
ChikaraHanakawa <hanakawa.c@gmail.com>
2023-11-23 01:06:43 +0900

    update Day12

---
+++ Day12/probex5-1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <string>
+int main(){
+    std::string input;
+    std::map <std::string ,std::string> jisho;
+    std::string eng[] = {"cat", "dog", "bird", "tiger"};
+    std::string jpn[] = {"猫", "犬", "鳥", "トラ"};
+    for(int i = 0; i < 4; i++){
+        jisho[eng[i]] = jpn[i];
+    }
+    std::cout << "英語で動物の名前を入力して下さい : ";
+    std::cin >> input;
+    if(jisho.count(input) != 0){
+        std::cout << jisho[input] << std::endl;
+    }else{
+        std::cout << "対応するデータは登録されていません" << std::endl;
+    }
+    return 0;
+}
