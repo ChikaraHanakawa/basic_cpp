45020e537c46a2778b0bd88b52b42091dcad6260
ChikaraHanakawa <hanakawa.c@gmail.com>
2024-04-23 09:47:28 +0900

    hotfix: Solved the problem of not being able to calculate the cube root

--- numcalc/root-PadeApproximation.cpp
+++ numcalc/root-PadeApproximation.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 class PadeApproximant{
-    double n, x, t;
+    double n, x, t, r;
     double Numerator;
     double Denominator;
     public:
@@ -10,12 +10,13 @@ class PadeApproximant{
         ~PadeApproximant();
         double getNum();
         void calc();
+        void square_square();
 };
 PadeApproximant::PadeApproximant(){
-    std::cout << "Constructor called" << std::endl;
+    std::cout << "This is a root-only Padé approximation." << std::endl;
 }
 PadeApproximant::~PadeApproximant(){
-    std::cout << "Destructor called" << std::endl;
+    std::cout << "Calclate complete!" << std::endl;
 }
 double PadeApproximant::getNum(){
     std::cout << "Enter the value of x, n, and t: ";
@@ -24,7 +25,6 @@ double PadeApproximant::getNum(){
     return x, n, t;
 }
 void PadeApproximant::calc(){
-    double r = std::sqrt(x);
     int Rounding_off_r = r;
     if(r - Rounding_off_r > 0.5){
         Rounding_off_r += 1;
@@ -35,10 +35,18 @@ void PadeApproximant::calc(){
     double ans = (Numerator * Rounding_off_r) / Denominator;
     std::cout << "= " << ans << std::endl;
 }
+void PadeApproximant::square_square(){
+    if(n == 2){
+        r = std::sqrt(x);
+    }else if(n == 3){
+        r = std::cbrt(x);
+    }
+}
 
 int main(){
     PadeApproximant p;
     p.getNum();
+    p.square_square();
     p.calc();
     return 0;
 }
